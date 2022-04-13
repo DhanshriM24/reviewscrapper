33ecd228a11edf1cd527893432eb1895325d694b
DhanshriM24 <manusmaredhanashri@gmail.com>
2022-04-14 01:06:49 +0530

    this is my 1st commit

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+    app.run(host='127.0.0.1', port=8001, debug=True)
+	#app.run(debug=True)
